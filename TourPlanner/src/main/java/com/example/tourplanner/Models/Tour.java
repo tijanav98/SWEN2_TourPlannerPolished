cb00964e8e7ff58f49e8e4e8857eff6577812b6f
tijanav98 <122912580+tijanav98@users.noreply.github.com>
2023-06-11 21:18:04 +0200

    Added Tour Class w lombok

---
+++ TourPlanner/src/main/java/com/example/tourplanner/Models/Tour.java
@@ -0,0 +1,20 @@
+package com.example.tourplanner.Model;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Tour {
+    private String name;
+    private String description;
+    private String from;
+    private String to;
+    private String transportType;
+    private double distance;
+    private String estimatedTime;
+    private String routeInfo;
+}
