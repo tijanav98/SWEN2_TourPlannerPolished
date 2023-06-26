94bb2ad31a5045a26a80262ecdfb52241c16bfcd
tijanav98 <122912580+tijanav98@users.noreply.github.com>
2023-06-26 09:00:18 +0200

    Updated Constructor and Setters to fix error

--- TourPlanner/src/main/java/com/example/tourplanner/DAL/models/Tour.java
+++ TourPlanner/src/main/java/com/example/tourplanner/DAL/models/Tour.java
@@ -1,13 +1,10 @@
 package com.example.tourplanner.DAL.models;
-import lombok.AllArgsConstructor;
+
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
 
 @Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class Tour {
     private int id;
     private String name;
@@ -18,4 +15,48 @@ public class Tour {
     private double distance;
     private String estimatedTime;
     private String routeInfo;
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public void setFrom(String from){
+        this.from = from;
+    }
+
+    public void setTo(String to){
+        this.to = to;
+    }
+
+    public void setTransportType(String transportType){
+        this.transportType = transportType;
+    }
+
+    public void setDistance(double distance){
+        this.distance = distance;
+    }
+
+    public void setEstimatedTime(String estimatedTime){
+        this.estimatedTime = estimatedTime;
+
+    }
+
+    public void setRouteInfo (String routeInfo){
+        this.routeInfo = routeInfo;
+    }
+    public Tour(int id, String name, String description, String from, String to, String transportType, double distance, String estimatedTime, String routeInfo) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.from = from;
+        this.to = to;
+        this.transportType = transportType;
+        this.distance = distance;
+        this.estimatedTime = estimatedTime;
+        this.routeInfo = routeInfo;
+    }
 }
